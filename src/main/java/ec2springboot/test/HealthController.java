0f7a2237b1f46dd4ed8e3436134743868ec470de
Anirudh938 <revallianirudh@gmail.com>
2025-06-19 19:53:35 +0530

    changes

--- src/main/java/ec2springboot/test/HealthController.java
+++ src/main/java/ec2springboot/test/HealthController.java
@@ -1,17 +1,12 @@
 package ec2springboot.test;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
-public class TestController {
-
+public class HealthController {
     @GetMapping("/")
-    public String test() {
-        return "Welcome";
+    public String health() {
+        return "OK";
     }
-
-
 }
