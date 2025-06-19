d76e300ef33bf5a1e08f1dd23d86bc12d2169269
Anirudh938 <revallianirudh@gmail.com>
2025-06-19 20:21:56 +0530

    test

--- src/main/java/ec2springboot/test/TestController.java
+++ src/main/java/ec2springboot/test/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class TestController {
 
-    @GetMapping("/")
+    @GetMapping(value = "/", produces = "text/plain")
     public ResponseEntity<String> test() {
         return ResponseEntity.ok("Welcome");
     }
